e11495e5c06ffdce17f70d1fe9489d47be5bbfd2
Elroy <lsaltzherr@gmail.com>
2022-04-08 16:14:25 -0700

    menu start

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,17 +1,35 @@
 class Menu extends Phaser.Scene {
-    constructor() {
-      super("menuScene");
-    }
-    
-    preload() {
-      // load audio
-      this.load.audio('sfx_select', './assets/assets_blip_select12.wav');
-      this.load.audio('sfx_explosion', './assets/assets_explosion38.wav');
-      this.load.audio('sfx_rocket', './assets/assets_rocket_shot.wav');
+  constructor() {
+    super("menuScene");
+  }
+
+  preload() {
+    // load audio
+    this.load.audio('sfx_select', './assets/assets_blip_select12.wav');
+    this.load.audio('sfx_explosion', './assets/assets_explosion38.wav');
+    this.load.audio('sfx_rocket', './assets/assets_rocket_shot.wav');
+  }
+
+  create() {
+    let menuConfig = {
+      fontFamily: 'Courier',
+      fontSize: '28px',
+      backgroundColor: '#F3B141',
+      color: '#843605',
+      align: 'right',
+      padding: {
+        top: 5,
+        bottom: 5,
+      },
+      fixedWidth: 0
     }
 
-    create() {
-      this.add.text(20, 20, "Rocket Patrol Menu");
-      this.scene.start("playScene");
-    }
+    this.add.text(game.config.width/2, game.config.height/2 + borderUISize - borderPadding, "ROCKET PATROL", menuConfig).setOrigin(0.5);
+    this.add.text(game.config.width/2, game.config.height/2, "use <--> arrows to move & (F) to fire", menuConfig).setOrigin(0.5);
+    menuConfig.backgroundColor = "#00FF00";
+    menuConfig.color = "#000";
+    this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig). setOrigin(0.5);
+
+
   }
+}
