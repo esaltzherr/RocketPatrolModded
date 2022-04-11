2e58aa5f496cd4bd6dcae93391d05fdf83fd3c25
Elroy <lsaltzherr@gmail.com>
2022-04-10 19:29:47 -0700

    empty commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -76,7 +76,7 @@ class Play extends Phaser.Scene {
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
             this.scene.start("menuScene");
         }
-        this.starfield.tilePositionX -= 4;
+        this.starfield.tilePositionX -= 4; 
         if (!this.gameOver) {
             this.p1Rocket.update();         // update rocket sprite
             this.ship01.update();           // update spaceships (x3)
